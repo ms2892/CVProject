a583c644867fea06bc3cd2e9dcaab7db97e34c31
ms2892 <ms2892@cam.ac.uk>
2022-12-26 17:56:42 +0000

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Mini Project for MLMI12 University of Cambridge',
+    author='Mohd Sadiq',
+    license='',
+)
