ba8c73fdbaf641133c4359dcf1d2f7c143b64a7d
ms2892 <ms2892@cam.ac.uk>
2022-12-28 00:48:37 +0000

    Added DataLoader and Generated Pairs

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,17 +3,67 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+from glob import glob
+import random
+import pickle
+from tqdm import tqdm
 
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+# @click.command()
+# @click.argument('input_filepath', type=click.Path(exists=True))
+# @click.argument('output_filepath', type=click.Path())
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    train_files = glob('../../data/raw/tiny-imagenet-200/train/*/images/*')
+    class_labels = glob('../../data/raw/tiny-imagenet-200/train/*')
+    classes={}
+    for i in range(len(class_labels)):
+        class_labels[i]=class_labels[i].replace('\\','/')
+        split = class_labels[i].split('/')
+        classes[split[6]]=[]
+    # print(classes)
+    for i in range(len(train_files)):
+        train_files[i]=train_files[i].replace('\\','/')
+        split = train_files[i].split('/')
+        classes[split[6]].append(train_files[i])
+    labels={'sim':[],'oth':[]}
+    flag_sim=True
+    flag_oth=True
+    cnt_sim=0
+    cnt_oth=0
+    
+    with tqdm(total=100000) as pbar:
+        while(flag_sim or flag_oth):
+            ele = random.choices(train_files,k=2)
+            split1 = ele[0].split('/')
+            class1 = split1[6]
+            split2 = ele[1].split('/')
+            class2 = split2[6]
+            
+            if class1==class2 and flag_sim:
+                labels['sim'].append(ele)
+                pbar.update(1)
+                cnt_sim+=1
+                if cnt_sim==50000:
+                    flag_sim=False
+            elif class1!=class2 and flag_oth:
+                labels['oth'].append(ele)
+                cnt_oth+=1
+                pbar.update(1)
+                if cnt_oth==50000:
+                    flag_oth=False
+        # print(ele)/
+        # t=input()
+    print(len(labels['sim']),len(labels['oth']))
+    print(labels['sim'][0])
+    with open('../../data/processed/pairs.txt','wb') as handle:
+        pickle.dump(labels,handle)
+    
+    # print(train_files)
+    
+    # logger = logging.getLogger(__name__)
+    # logger.info('making final data set from raw data')
 
 
 if __name__ == '__main__':
@@ -25,6 +75,6 @@ if __name__ == '__main__':
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    # load_dotenv(find_dotenv())
 
     main()
--- src/models/dataloader.py
+++ src/models/dataloader.py
@@ -1,11 +1,73 @@
 import torch
 import torchvision
+from torchvision import transforms
 from torch.utils.data import Dataset,DataLoader
 import torch.nn as nn
 import numpy as np
 import math
+from glob import glob
+import pickle
+from torchvision.transforms.functional import InterpolationMode
+import cv2
 
 class TinyImageNetLoader(Dataset):
     
-    def __init__(self,path,transform=None):
+    def __init__(self,transform=None):
+        super(TinyImageNetLoader,self).__init__()
+        with open('../../data/processed/pairs.txt','rb') as handle:
+            self.pairs = pickle.loads(handle.read())
+        self.x = [i for i in self.pairs['sim']]
+        self.y = ['sim']*50000
         
+        self.x += [i for i in self.pairs['oth']]
+        self.y += ['oth']*50000
+        self.transform = transform
+        
+    def __getitem__(self, index):
+        img1, img2 = self.get_images(self.x[index])
+        return (img1,img2), self.y[index]
+        
+    def get_images(self,ele):
+        pth1 = ele[0]
+        pth2 = ele[1]
+        
+        img1 = cv2.imread(pth1)
+        img2 = cv2.imread(pth2)
+        
+        img1 = cv2.cvtColor(img1,cv2.COLOR_BGR2RGB)
+        img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
+        
+        img1 = np.moveaxis(img1,-1,0)
+        img2 = np.moveaxis(img2,-1,0)
+        
+        img1 = torch.from_numpy(img1)
+        img2 = torch.from_numpy(img2)
+        
+        img1 = img1.type(torch.uint8)
+        img2 = img2.type(torch.uint8)
+        
+        img1 = self.transform(img1)
+        img2 = self.transform(img2)
+        
+        return img1,img2
+    
+if __name__=='__main__':
+    compose = transforms.Compose([transforms.Resize((384,384),interpolation=InterpolationMode.BICUBIC), transforms.RandAugment(num_ops=2,magnitude=0)])
+    dataset = TinyImageNetLoader(compose)
+    
+    imgs, label = dataset[2000]
+    
+    img1 = imgs[0].numpy()
+    img1 = np.moveaxis(img1,0,-1)
+    img1 = cv2.cvtColor(img1,cv2.COLOR_RGB2BGR)
+    
+    img2 = imgs[1].numpy()
+    img2 = np.moveaxis(img2,0,-1)
+    img2 = cv2.cvtColor(img2,cv2.COLOR_RGB2BGR)
+    
+    
+    
+    cv2.imshow(label,img1)
+    cv2.waitKey(0)
+    cv2.imshow(label,img2)
+    cv2.waitKey(0)
